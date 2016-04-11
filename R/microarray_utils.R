6fa8079d587b82edcabf9e51e7cd22297db0a09f
nickilott <nicholas.ilott@kennedy.ox.ac.uk>
2016-04-11 21:14:16 +0100

    added plots for fold change scatters

--- R/microarray_utils.R
+++ R/microarray_utils.R
@@ -9,7 +9,11 @@
 # load packages
 library("ggplot2")
 library("grid")
+library("plyr")
 
+##########################################
+##########################################
+##########################################
 
 readData <- function(tsv, header=TRUE){
 	 return (read.csv(tsv,
@@ -17,6 +21,10 @@ readData <- function(tsv, header=TRUE){
 			 stringsAsFactors=F,
 			 sep="\t"))
 			 }
+
+##########################################
+##########################################
+##########################################
 
 filterNonUniqueGenes <- function(probe2gene){
                             genes = c()
@@ -45,7 +53,6 @@ filterNonUniqueGenes <- function(probe2gene){
 ##########################################
 ##########################################
 
-
 filterNonUniqueProbes <- function(probe2gene){
                             genes = c()
                             probes = c()
@@ -68,13 +75,11 @@ filterNonUniqueProbes <- function(probe2gene){
 			    probe2gene[,1] <- as.character(probe2gene[,1])
 			    probe2gene[,2] <- as.character(probe2gene[,2])	
 			    return(na.omit(probe2gene))
-                            }
-
+}
 
 ##########################################
 ##########################################
 ##########################################
-
 
 runPCA <- function(matrix.file){
 
@@ -88,12 +93,11 @@ runPCA <- function(matrix.file){
 
        # return pca object
        return (pc.dat)
-       }
+}
 
 ##########################################
 ##########################################
 ##########################################
-
 
 plotPCA <- function(pc.dat,
                     pcs=c("PC1", "PC2"),
@@ -123,11 +127,9 @@ plotPCA <- function(pc.dat,
       ggsave(file = outfile, limitsize = F)
       }
 
-
 ##########################################
 ##########################################
 ##########################################
-
 
 plotLoadings <- function(pc.dat,
 	                 probe2gene,
@@ -182,11 +184,70 @@ plotLoadings <- function(pc.dat,
 	       
 }
 
+##########################################
+##########################################
+##########################################
+
+summariseData <- function(dat, cond1, cond2){
+	         df <- ddply(dat, c("probe", "cond"), summarise, average=mean(value))
+	         df <- data.frame(cbind(df[df$cond == cond1,],
+	                                   df[df$cond == cond2,]))
+		 return(df)
+}
 
 ##########################################
 ##########################################
 ##########################################
 
+annotateGenes <- function(df, anno){
+    		# annotate genes
+        	df$status <- ifelse(df$probe %in% anno$probe, "annotated", "not changed")
+		for (p in anno$probe){
+	            df$annotation[df$probe == p] <- anno$gene[anno$probe == p]
+		    }
+		return (df)
+
+}
+
+##########################################
+##########################################
+##########################################
+
+getClusterColours <- function(clusters){
+	             # get colours associated with cluster
+	             clusters$colour <- rainbow(max(clusters$cluster),
+		                                s=1,
+						v=0.75)[clusters$cluster]
+		     return(clusters)
+}
+
+##########################################
+##########################################
+##########################################
+
+addColours <- function(df, clustersWithColours){
+	      # add colours
+	      df$probe <- as.character(df$probe)
+	      df$colour <- clustersWithColours[df$probe,]$colour
+	      df$colour <- ifelse(is.na(df$colour), "grey", df$colour)
+	      return(df)
+}
+
+##########################################
+##########################################
+##########################################
+
+addClusters <- function(df, clusters){
+		df$cluster <- clusters[df$probe,]$cluster
+		df$cluster <- ifelse(is.na(df$cluster),
+		                     "not changed",
+				      df$cluster)
+		return(df)
+}
+
+##########################################
+##########################################
+##########################################
 
 scatterplotAbundances <- function(matrix.file,
                                   annotation,
@@ -216,31 +277,24 @@ scatterplotAbundances <- function(matrix.file,
 			 dat$cond <- unlist(strsplit(as.character(dat$variable), ".R[0-9]"))
 
 			 # summarise data per probe/condition
-			 dat.summarised <- ddply(dat, c("probe", "cond"), summarise, average=mean(value))
-	    		 dat.summarised <- data.frame(cbind(dat.summarised[dat.summarised$cond == cond1,],
-	                                                dat.summarised[dat.summarised$cond == cond2,]))
+			 dat.summarised <- summariseData(dat, cond1, cond2)
 
-    			# annotate genes
-        		dat.summarised$status <- ifelse(dat.summarised$probe %in% anno$probe, "annotated", "not changed")
+       			 # annotate genes
+			 dat.summarised <- annotateGenes(dat.summarised, anno)
 
-			for (p in anno$probe){
-	                     dat.summarised$annotation[dat.summarised$probe == p] <- anno$gene[anno$probe == p]}
-
-			# scatterplot the results
-                        plot1 <- ggplot(dat.summarised, aes(x=average, y=average.1, colour=status, label=annotation))
-	                plot2 <- plot1 + geom_point(shape=18) + xlab(cond1) + ylab(cond2)
-			plot3 <- plot2 + geom_abline(slope=1, intercept=1, linetype="dashed")
-			plot4 <- plot3 + geom_abline(slope=1, intercept=-1, linetype="dashed") 
-			plot5 <- plot4 + geom_abline(slope=1, intercept=0)
-			plot5 + geom_text() + scale_colour_manual(values=c("red", "grey"))
-		        ggsave(outfile)
-		    }
-
+			 # scatterplot the results
+                         plot1 <- ggplot(dat.summarised, aes(x=average, y=average.1, colour=status, label=annotation))
+	                 plot2 <- plot1 + geom_point(shape=18) + xlab(cond1) + ylab(cond2)
+			 plot3 <- plot2 + geom_abline(slope=1, intercept=1, linetype="dashed")
+			 plot4 <- plot3 + geom_abline(slope=1, intercept=-1, linetype="dashed") 
+			 plot5 <- plot4 + geom_abline(slope=1, intercept=0)
+			 plot5 + geom_text() + scale_colour_manual(values=c("red", "grey"))
+		         ggsave(outfile)
+}
 
 ##########################################
 ##########################################
 ##########################################
-
 
 scatterplotAbundancesClusters <- function(matrix.file,
                                   annotation,
@@ -265,7 +319,7 @@ scatterplotAbundancesClusters <- function(matrix.file,
 			 rownames(clusters) <- clusters$probe
 
 			 # get colours associated with cluster
-			 clusters$colour <- rainbow(max(clusters$cluster), s=1, v=0.75)[clusters$cluster]
+			 clusters <- getClusterColours(clusters)
 
 			 # get conditions
 			 conds <- unlist(strsplit(colnames(dat), ".R[0-9]"))
@@ -278,37 +332,102 @@ scatterplotAbundancesClusters <- function(matrix.file,
 			 dat$cond <- unlist(strsplit(as.character(dat$variable), ".R[0-9]"))
 
 			 # summarise data per probe/condition
-			 dat.summarised <- ddply(dat, c("probe", "cond"), summarise, average=mean(value))
-	    		 dat.summarised <- data.frame(cbind(dat.summarised[dat.summarised$cond == cond1,],
-	                                                dat.summarised[dat.summarised$cond == cond2,]))
+			 dat.summarised <- summariseData(dat, cond1, cond2)
 
-    			# annotate genes
-        		dat.summarised$status <- ifelse(dat.summarised$probe %in% anno$probe, "annotated", "not changed")
+    			 # annotate genes
+        		 dat.summarised <- annotateGenes(dat.summarised, anno)
 
-			for (p in anno$probe){
-	                     dat.summarised$annotation[dat.summarised$probe == p] <- anno$gene[anno$probe == p]}
-			rownames(dat.summarised) <- dat.summarised$probe
+			 # add colours
+			 dat.summarised <- addColours(dat.summarised, clusters)
+
+			 # add clusters
+			 dat.summarised <- addClusters(dat.summarised, clusters)
+ 
+			 # order for colours to work
+		 	 dat.summarised <- dat.summarised[order(dat.summarised$cluster),]
+
+			 # get size - big for in cluster
+			 dat.summarised$size <- ifelse(dat.summarised$cluster == "not changed", 1, 4)
+
+			 # scatterplot the results
+			 col <- factor(dat.summarised$cluster, levels = sort(unique(dat.summarised$cluster)))
+                         plot1 <- ggplot(dat.summarised, aes(x=average, y=average.1, colour=col, label=annotation, stat="identity", size=size))
+	                 plot2 <- plot1 + geom_point(shape=18) + xlab(cond1) + ylab(cond2)
+			 plot3 <- plot2 + geom_abline(slope=1, intercept=1, linetype="dashed")
+			 plot4 <- plot3 + geom_abline(slope=1, intercept=-1, linetype="dashed") 
+			 plot5 <- plot4 + geom_abline(slope=1, intercept=0)
+			 plot5 + geom_text(colour="black") + scale_colour_manual(values=unique(dat.summarised$colour))
+		         ggsave(outfile)
+}
+
+##########################################
+##########################################
+##########################################
+
+scatterplotFoldChanges <- function(results1,
+		                  results2,	
+		        	  annotation,
+		       		  clusters,
+		       		  outfile="scatterplot_fold.pdf"){
+
+		       # read and format
+                       results1 <- readData(results1)
+		       results2 <- readData(results2)
+		       rownames(results1) <- results1$ID
+		       rownames(results2) <- results2$ID
+
+		       # combine into single dataframe
+                       results1 <- results1[rownames(results2),]
+
+		       results <- data.frame(cbind(results1, results2))
+		       results$probe <- results$ID.1
+
+		       # read annotations
+		       anno <- readData(annotation)
+
+                       # read clusters
+		       clusters <- readData(clusters)
+		       rownames(clusters) <- clusters$probe
+
+    		       # annotate genes
+		       results <- annotateGenes(results, anno)
+		       rownames(results) <- results$probe
+
+		       # add colours
+		       clusters <- getClusterColours(clusters)
+		       results <- addColours(results, clusters)	
+
+		       # assign clusters
+		       results <- addClusters(results, clusters)
+
+		       # order for colours to work
+		       results <- results[order(results$cluster),]
+
+		       # get size - big for in cluster
+		       results$size <- ifelse(results$cluster == "not changed", 1, 2)
+
+		       # plot
+		       maximum <- max(append(results$logFC, results$logFC.1))
+		       minimum <- min(append(results$logFC, results$logFC.1))
+
+		       plot1 <- ggplot(results, aes(x=logFC, y=logFC.1, colour=colour, label=annotation, stat="identity", size=size))
+		       plot2 <- plot1 + geom_point(shape=18) + scale_size_area(max_size=3)
+		       plot3 <- plot2 + geom_text(colour="black", size=5) + scale_colour_manual(values=unique(results$colour))
+		       plot4 <- plot3 + xlim(minimum, maximum) + ylim(minimum, maximum)
+		       plot5 <- plot4 + geom_hline(yintercept=c(-1,1), linetype="dashed") + geom_vline(xintercept=c(-1,1), linetype="dashed")
+		       plot5 
+		       ggsave(outfile)
+}
+
+##########################################
+##########################################
+##########################################
 
 
-			# add colours
-			dat.summarised$colour <- clusters[dat.summarised$probe,]$colour
-			dat.summarised$colour <- ifelse(is.na(dat.summarised$colour), "grey", dat.summarised$colour)
-			dat.summarised$cluster <- clusters[dat.summarised$probe,]$cluster
-			dat.summarised$cluster <- ifelse(is.na(dat.summarised$cluster), "not changed", dat.summarised$cluster)
-
-			# order for colours to work
-			dat.summarised <- dat.summarised[order(dat.summarised$cluster),]
-
-			# get size - big for in cluster
-			dat.summarised$size <- ifelse(dat.summarised$cluster == "not changed", 1, 4)
-
-			# scatterplot the results
-			col <- factor(dat.summarised$cluster, levels = sort(unique(dat.summarised$cluster)))
-                        plot1 <- ggplot(dat.summarised, aes(x=average, y=average.1, colour=col, label=annotation, stat="identity", size=size))
-	                plot2 <- plot1 + geom_point(shape=18) + xlab(cond1) + ylab(cond2)
-			plot3 <- plot2 + geom_abline(slope=1, intercept=1, linetype="dashed")
-			plot4 <- plot3 + geom_abline(slope=1, intercept=-1, linetype="dashed") 
-			plot5 <- plot4 + geom_abline(slope=1, intercept=0)
-			plot5 + geom_text(colour="black") + scale_colour_manual(values=unique(dat.summarised$colour))
-		        ggsave(outfile)
-		    }
+plotPathways <- function(pathway_results, colour="red", outfile="pathways.pdf"){
+	     dat <- readData(pathway_results)
+	     plot1 <- ggplot(dat, aes(x=description, y=ratio))
+	     plot2 <- plot1 + geom_bar(stat="identity", fill=colour)
+	     plot2 + scale_fill_manual(values=c(colour)) + coord_flip()
+	     ggsave(outfile)
+}
